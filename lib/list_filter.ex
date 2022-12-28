9cc2c6bcf105e0ffe8f127abca8baa1362d6dc06
igor jonny <igorjonny1@gmail.com>
2022-12-28 19:06:00 -0300

    adicionar text

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,16 +1,19 @@
 defmodule ListFilter do
   require Integer
 
+  # pega a lista e filtra o numero e depois conta
   def call(list) do
     list
     |> Enum.filter(fn elem -> is_an_odd_num?(elem) end)
-    |> Enum.count
+    |> Enum.count()
   end
 
-  def is_an_odd_num?(string) do
+  # aqui pega se na lista tem uma string case for uma string
+  # converto ela num numero
+  defp is_an_odd_num?(string) do
     case Integer.parse(string) do
       {num, _} -> Integer.is_odd(num)
       _ -> false
     end
-  end 
+  end
 end
